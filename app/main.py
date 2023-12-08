274cf92505ebdb1cc4231437ca705ae657c3fdb3
Arthur <arthur.nguyen@efrei.net>
2023-12-08 15:21:50 +0100

    set up text sumarization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from .controllers import speech_to_text
+from .controllers import speech_to_text, text_summarization
 
 app = FastAPI()
 
 app.include_router(speech_to_text.router)
+app.include_router(text_summarization.router)
