ba230f8547277fa6d62f5b67f05c0d7fd3b569e9
Arthur <arthur.nguyen@efrei.net>
2024-01-24 23:12:00 +0100

    fix on data request changes

--- app/controllers/summaries.py
+++ app/controllers/summaries.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException, File, UploadFile
-from ..services.summaries import add_summary as add_summary_service
+from ..services.summaries import SummaryRequest, add_summary as add_summary_service
 
 router = APIRouter()
 
 
 @router.post("/summaries", tags=["summaries"], status_code=200)
-async def add_summary(input_file: UploadFile = File(...)):
+async def add_summary(request_file: SummaryRequest):
     try:
-        summary = await add_summary_service(input_file)
+        summary = await add_summary_service(request_file)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return {"summary": summary}
