e41c4c5b356c1d358d77ef2c5498b84eb85df942
Kylian Sauvee <kylian@sauvee.com>
2024-01-30 14:04:46 +0100

    feat(redis_cache): add pub_sub and expire_handler

--- app/redis_cache.py
+++ app/redis_cache.py
@@ -3,7 +3,34 @@ from redis import ResponseError
 from redis.commands.search.field import TextField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 
-redis_client = redis.Redis(host='redis', port=6379, decode_responses=True)
+from app.database.database import insert_summary_in_db
+
+shadow_key = "shadowKey:"
+
+
+def expire_handler(message):
+    try:
+        key = message['data']
+        print(key)
+        if shadow_key in key:
+            key = key.replace(shadow_key, "")
+
+            if "transcript:" in key:
+                redis_client.delete(key)
+            else:
+                summary = redis_client.json().get(key)
+                redis_client.delete(key)
+                insert_summary_in_db(key, summary)
+    except Exception as e:
+        print(e)
+
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+pub_sub = redis_client.pubsub()
+pub_sub.subscribe(**{'__keyevent@0__:expired': expire_handler})
+pub_sub.run_in_thread(sleep_time=0.01)
+
 schema = (
     TextField("$.fileName", as_name="fileName"),
     TextField("$.data", as_name="data"),
